39b2b7e3855e484dba9f281e7111f725b516eef6
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-02-20 12:17:50 -0500

    commit tree d9f0b96af38aac4bc84be9f297e17c2c0f0c4602

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,31 @@
+require 'pry'
+class Doctor
+    attr_accessor :name, :appointments, :patient
 
+    @@all = []
+
+    def initialize(name)
+      @name = name
+      @appointments = []
+      @@all << self
+    end
+  
+    def self.all
+        @@all
+    end
+
+    def new_appointment(patient, date)
+       appointment = Appointment.new(patient, date, self)
+       @appointments << appointment
+       appointment
+    end
+  
+    def patients
+        appointments.map { |appt| appt.patient}
+    end
+
+    def appointments
+        Appointment.all.select {|app| app.doctor == self}
+    end
+
+  end
